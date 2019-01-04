5d8a0936d3203acf72684579b537d75744e5a2fb
ThreeDPrinter <manuel.stoer@outlook.com>
2019-01-04 20:32:40 +0100

    Removed comments

--- src/Gedit/src/Main.cpp
+++ src/Gedit/src/Main.cpp
@@ -1,9 +1,3 @@
-/* Commands:
- * V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2.
- * H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2.
- * F X Y C - Fill the region R with the colour C. R is defined as: Pixel (X,Y) belongs to R. Any other pixel which is the same colour as (X,Y) and shares a common side with any pixel in R also belongs tso this region.
- */
-
 #include "Gedit.h"
 
 int main() {
